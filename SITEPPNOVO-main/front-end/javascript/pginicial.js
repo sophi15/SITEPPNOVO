c5e69a7f80ad7419ca82ac039d1164c40b08951d
sophi15 <84184842+sophi15@users.noreply.github.com>
2023-09-28 11:25:06 -0300

    Add files via upload

--- SITEPPNOVO-main/front-end/javascript/pginicial.js
+++ SITEPPNOVO-main/front-end/javascript/pginicial.js
@@ -3,11 +3,11 @@ var cardcontainer = document.getElementById("cardcontainer");
 var cardsobre = document.getElementById("cardsobre");
 
 function mostrarCard() {
-    cardcontainer.style.left = "0"; /* Move o card para a posição visível */
+    cardcontainer.style.right = "0"; /* Move o card para a posição visível */
 }
 
 function esconderCard() {
-    cardcontainer.style.left = "-300px"; /* Move o card para fora da tela */
+    cardcontainer.style.right = "-300px"; /* Move o card para fora da tela */
 }
 
 document.getElementById("sobrecard").addEventListener("mouseenter", mostrarCard);
