56929849045655613190b7d8f4ad4e91dba144e5
sophi15 <84184842+sophi15@users.noreply.github.com>
2023-10-18 23:57:27 -0300

    Add files via upload

---
+++ front-end/paginicial/Botoes.js
@@ -0,0 +1,9 @@
+// botao pg sobre
+document.getElementById("PaginaSobre").addEventListener("click", function() {
+    window.location.replace("../sobre/Sobre.html");
+});
+
+// botao dos coments
+document.getElementById("botaoComentarios").addEventListener("click", function() {
+    window.location.href = "#comentariosSection";
+});
